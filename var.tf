41dd1425464a994e06bf7f293c5ee3e8673fd55b
Srilakshmi Doma <60988616+SrilakshmiDoma14@users.noreply.github.com>
2020-08-20 17:23:44 -0400

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,16 @@
+# Variables TF File
+
+variable "access_key" {
+  description = "AWS Access Key"
+  default     = ""
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  default     = ""
+}
+
+variable "region" {
+  description = "AWS Region "
+  default     = "us-east-2"
+}
