964dc3f6f74641561a61bf84706771788297f4a5
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-09-18 07:22:15 -0700

    Update and rename create-ami.sh to build-ami.py

--- create-ami.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ARTIFACT=`packer build -machine-readable gold-ami.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
-
-echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.txt
-echo $AMI_ID
-
-
