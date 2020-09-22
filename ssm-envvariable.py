583733ba6234173194964b084af9af722ff0ba7c
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-09-22 11:40:31 -0700

    Update ssm-envvariable.py

--- ssm-envvariable.py
+++ ssm-envvariable.py
@@ -7,19 +7,11 @@ from packerpy import PackerExecutable
 import os 
 import sys
 from botocore.exceptions import ClientError
-
-BUCKET_NAME = 'demo-s3lambda'
+try:
+    aws_session = boto3.Session()
+    lambda_client = aws_session.client('lambda')
+except botocore.exceptions.NoRegionError as e: 
+    pass
 
 def lambda_handler(event, context):
-    ssm = boto3.client('ssm')
-    ssm_parameter = ssm.get_parameter(Name='/GoldenAMI/Linux/RedHat-7/source', WithDecryption=True)
-    save_parameter = (ssm_parameter['Parameter']['Value'])
-    print (save_parameter)
-    print('This message will be displayed on the screen.')
-    original_stdout = sys.stdout # Save a reference to the original standard output
-    with open('/tmp/var.txt', 'w') as f:
-        sys.stdout = original_stdout
-        f = open("/tmp/var.txt", "r")
-        f.readline()
-    subprocess.call("set /P id=</tmp/var.txt", shell=True)
-    print(os.environ)
+    invoke_response = lambda_client.invoke(FunctionName="Second_function_name",InvocationType='RequestResponse')
